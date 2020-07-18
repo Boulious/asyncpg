0715bf8d218867cb78c3042b2987ef0bada41ca0
Elvis Pranskevichus <elvis@magic.io>
2020-07-18 14:07:38 -0700

    Allow using custom Record class
    Add the new `record_class` parameter to the `create_pool()` and
    `connect()` functions, as well as to the `cursor()`, `prepare()`,
    `fetch()` and `fetchrow()` connection methods.
    
    This not only allows adding custom functionality to the returned
    objects, but also assists with typing (see #577 for discussion).
    
    Fixes: #40.

--- asyncpg/protocol/record/recordobj.c
+++ asyncpg/protocol/record/recordobj.c
@@ -15,9 +15,14 @@ static PyObject * record_new_items_iter(PyObject *);
 static ApgRecordObject *free_list[ApgRecord_MAXSAVESIZE];
 static int numfree[ApgRecord_MAXSAVESIZE];
 
+static size_t MAX_RECORD_SIZE = (
+    ((size_t)PY_SSIZE_T_MAX - sizeof(ApgRecordObject) - sizeof(PyObject *))
+    / sizeof(PyObject *)
+);
+
 
 PyObject *
-ApgRecord_New(PyObject *desc, Py_ssize_t size)
+ApgRecord_New(PyTypeObject *type, PyObject *desc, Py_ssize_t size)
 {
     ApgRecordObject *o;
     Py_ssize_t i;
@@ -27,21 +32,31 @@ ApgRecord_New(PyObject *desc, Py_ssize_t size)
         return NULL;
     }
 
-    if (size < ApgRecord_MAXSAVESIZE && (o = free_list[size]) != NULL) {
-        free_list[size] = (ApgRecordObject *) o->ob_item[0];
-        numfree[size]--;
-        _Py_NewReference((PyObject *)o);
-    }
-    else {
-        /* Check for overflow */
-        if ((size_t)size > ((size_t)PY_SSIZE_T_MAX - sizeof(ApgRecordObject) -
-                    sizeof(PyObject *)) / sizeof(PyObject *)) {
+    if (type == &ApgRecord_Type) {
+        if (size < ApgRecord_MAXSAVESIZE && (o = free_list[size]) != NULL) {
+            free_list[size] = (ApgRecordObject *) o->ob_item[0];
+            numfree[size]--;
+            _Py_NewReference((PyObject *)o);
+        }
+        else {
+            /* Check for overflow */
+            if ((size_t)size > MAX_RECORD_SIZE) {
+                return PyErr_NoMemory();
+            }
+            o = PyObject_GC_NewVar(ApgRecordObject, &ApgRecord_Type, size);
+            if (o == NULL) {
+                return NULL;
+            }
+        }
+
+        PyObject_GC_Track(o);
+    } else {
+        assert(PyType_IsSubtype(type, &ApgRecord_Type));
+
+        if ((size_t)size > MAX_RECORD_SIZE) {
             return PyErr_NoMemory();
         }
-        o = PyObject_GC_NewVar(ApgRecordObject, &ApgRecord_Type, size);
-        if (o == NULL) {
-            return NULL;
-        }
+        o = (ApgRecordObject *)type->tp_alloc(type, size);
     }
 
     for (i = 0; i < size; i++) {
@@ -51,7 +66,6 @@ ApgRecord_New(PyObject *desc, Py_ssize_t size)
     Py_INCREF(desc);
     o->desc = (ApgRecordDescObject*)desc;
     o->self_hash = -1;
-    PyObject_GC_Track(o);
     return (PyObject *) o;
 }
 
